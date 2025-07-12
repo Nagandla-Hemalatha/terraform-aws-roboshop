c45341108ef8bd363020338fe8014d44d2ac6b08
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-07-12 07:20:58 +0530

    terraform components setup module started

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z040983115KD520HXV305"
+}
+
+variable "zone_name" {
+    default = "hemalatha.site"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
